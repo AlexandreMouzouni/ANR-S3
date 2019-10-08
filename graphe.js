50b99e9ace6956f48a0c151c84c756a0225f4800
xy2 <xy2@cock.li>
2019-10-08 13:38:25 +0000

    Arc en ciel ^__^

--- graphe.js
+++ graphe.js
@@ -1,4 +1,3 @@
-
 export default function graphe(data) {
 console.log(data);
 
@@ -11,10 +10,10 @@ const svg = d3.select('#graph');
 const chart = svg.append('g')
     .attr('transform', `translate(${margin}, ${margin})`);
 
-var nombres = data.map((d) => d.nombre) 
+var nombres = data.map((d) => d.nombre)
 // On obtient chaque nombre du tableau,
 // et on le stocke dans un autre tableau [123, 124, 125..]
-var nombreMax = Math.max(...nombres) 
+var nombreMax = Math.max(...nombres)
 // Obtenir le maximal pour faire l'axe Y de notre tableau
 
 const yScale = d3.scaleLinear()
@@ -24,10 +23,19 @@ const yScale = d3.scaleLinear()
 chart.append('g')
     .call(d3.axisLeft(yScale));
 
-const xScale = d3.scaleBand()	
+const xScale = d3.scaleBand()
     .range([0, width])
     .domain(data.map((d) => d.annee))
-    .padding(0.2)
+    .padding(0);
+
+console.log(xScale(1854))
+console.log(xScale(1864))
+console.log(xScale(1874))
+console.log(xScale(1884))
+
+var sequentialScale = d3.scaleSequential()
+    .domain([0, 10])
+    .interpolator(d3.interpolateRainbow);
 
 chart.append('g')
     .attr('transform', `translate(0, ${height})`)
@@ -41,5 +49,26 @@ chart.selectAll()
     .attr('y', (d) => yScale(d.nombre))
     .attr('height', (d) => height - yScale(d.nombre))
     .attr('width', xScale.bandwidth())
+    .style("stroke", "red")
+    .style('fill', (d,i) => sequentialScale(i))
 
+console.log(data.length)
+
+chart.selectAll()
+   .data(data)
+   .enter()
+   .append('text')
+   .text(function(d) {
+        return d.nombre;
+   })
+   .attr("x", function(d, i) {
+        return xScale(d.annee) + (xScale.bandwidth() / 2);  // +5
+   })
+   .attr("y", function(d) {
+        return yScale(d.nombre);              // +15
+   })
+   .attr("font-family", "sans-serif")
+   .attr("font-size", "11px")
+   .attr("fill", (d,i) => sequentialScale(i))
+   .attr("text-anchor", "middle");
 }
