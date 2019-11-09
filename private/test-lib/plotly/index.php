785efdbe23f67575f6e28118702cdcec8ac51f95
Lucas B <berlem.lucas@gmail.com>
2019-11-09 19:12:19 +0000

    Add new file

---
+++ private/test-lib/plotly/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta charset="utf-8"/>
+        <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+    </head>
+    
+    <body>
+        <h1>Line Plot</h1>
+        <div id="line-plot"></div>
+        
+        <h1>Bar Charts</h1>
+        <div id="bar-chart"></div>
+
+        <h1>Pie Chart</h1>
+        <div id="pie-chart"></div>
+    </body>
+</html>
