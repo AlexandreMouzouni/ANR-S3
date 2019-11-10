e7be3443b50672b8a66e7f417c710152d1e9a396
Lucas B <berlem.lucas@gmail.com>
2019-11-10 00:30:57 +0000

    Update private/test-lib/plotly/index.php Deleted private/test-lib/plotly/data.php

--- private/test-lib/plotly/index.php
+++ private/test-lib/plotly/index.php
@@ -1,7 +1,7 @@
 <?php
-    require 'data.php';
+	require_once '../../../Model.php';
+	$model = Model::get_model();
 ?>
-
 <html>
     <head>
         <title>Test Plotly</title>
@@ -20,7 +20,20 @@
         
     </body>
     <script>
-        const data = <?php echo json_encode(dataPhp()) . ";"; ?>
+    <?php
+      $anneeMinMax = $model->getNbAnnee();
+      $anneeMin = $anneeMinMax['dateMin'];
+      $anneeMax = explode('-', $anneeMinMax['dateMax']);
+      $anneeMax = $anneeMax[2];
+      $tableauDonnee= array();
+      for($annee= $anneeMin; $annee< $anneeMax; $annee+=1){
+          $nbPublication= $model->getNbPublicationsPeriodes($annee, $annee+1);
+          $nbPublication = intval($nbPublication); // Transformer en int
+          $tableauDonnee[] = ["annee" => strval($annee),
+                              "nombre" => $nbPublication];
+      }
+      ?>
+      const data = <?php echo json_encode($tableauDonnee) . ";"; ?>
 
         var layout = {
             height: 400,
