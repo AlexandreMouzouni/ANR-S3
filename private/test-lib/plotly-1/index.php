5ce8f72df906e337a559ad2532f86ef1747cf939
AlexandreMouzouni <alexloli225@gmail.com>
2020-01-10 14:11:14 +0000

    Update index.php

--- private/test-lib/plotly-1/index.php
+++ private/test-lib/plotly-1/index.php
@@ -11,7 +11,6 @@
   <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
   <div id="container" style="max-width: 1000px; height: 600px; margin: 2em auto"></div>
   <script type="module">
-   <script>
     <?php
       $anneeMinMax = $model->getNbAnnee();
       $anneeMin = $anneeMinMax['dateMin'];
