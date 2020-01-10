fcebf3f43214a797a55bca63883774b0c2b199c3
Lucas B <berlem.lucas@gmail.com>
2020-01-10 14:04:54 +0000

    Modification du guide d'utilisation

--- web/graphique/index.php
+++ web/graphique/index.php
@@ -2255,7 +2255,11 @@
                 <li>Données les plus proches en survol</li>
                 <li>Comparaison entre données en survol</li>
             </ul>
+            Si il y a plusieurs données d'affichées sur un graphique, on peut également cliquer sur les cases colorées symbolisant la donnée pour la masquer. 
+
           </div>
+            
+            
 
           <div>
             Pour le graphe réseaux, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
