f383fb8f55e512f81f7a53880315adc93d46cae9
AlexandreMouzouni <alexloli225@gmail.com>
2020-01-10 13:51:04 +0000

    Update index.php

--- private/graph test/index.php
+++ private/graph test/index.php
@@ -2,4 +2,9 @@
 	require_once 'Model.php'; 
 	$model = Model::get_model();
 ?>
-
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+  <div id="container" style="max-width: 1000px; height: 600px; margin: 2em auto"></div>
+  <script type="module">
