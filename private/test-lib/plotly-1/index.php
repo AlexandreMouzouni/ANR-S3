823571182368124f4ac06dedb0d884af566f7bc4
AlexandreMouzouni <alexloli225@gmail.com>
2020-01-10 14:07:58 +0000

    Update index.php

--- private/test-lib/plotly-1/index.php
+++ private/test-lib/plotly-1/index.php
@@ -11,3 +11,19 @@
   <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
   <div id="container" style="max-width: 1000px; height: 600px; margin: 2em auto"></div>
   <script type="module">
+   <script>
+    <?php
+      $anneeMinMax = $model->getNbAnnee();
+      $anneeMin = $anneeMinMax['dateMin'];
+      $anneeMax = explode('-', $anneeMinMax['dateMax']);
+      $anneeMax = $anneeMax[2];
+      $tableauDonnee= array();
+      for($annee= $anneeMin; $annee< $anneeMax; $annee+=1){
+          $nbPublication= $model->getNbPublicationsPeriodes($annee, $annee+1);
+          $nbPublication = intval($nbPublication); // Transformer en int
+          $tableauDonnee[] = ["annee" => strval($annee),
+                              "nombre" => $nbPublication];
+      }
+      ?>
+      const data = <?php echo json_encode($tableauDonnee) . ";"; ?>
+
