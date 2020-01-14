d8efe9b8bc6577919519d65623010619c4caedf1
AlexandreMouzouni <alexloli225@gmail.com>
2020-01-14 22:02:13 +0000

    Add new file

---
+++ private/test-lib/plotly-3/index.php
@@ -0,0 +1,50 @@
+<?php
+	require_once '../../Model.php';
+	$model = Model::get_model();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Dataviz</title>
+	<!-- Resources -->
+	<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+</head>
+<body>
+	<h1>Dataviz</h1>
+
+	<?php
+  $listauteur= $model->getNbOeuvreWithAuteur()
+
+  		$liste= [];
+  		foreach ($listauteur as $value) {
+  			$liste[] = $value["auteur"=>"x",
+                            "nombre"=>"y"];
+  		}
+
+  	 ?>
+ const data = <?php echo json_encode($liste) . ";"; ?>
+ var data = [{
+   type: 'scatterpolar',
+   r: donnee.map((d) => d.nombre),
+   theta: donnee.map((d) => d.auteur),
+   fill: 'toself',
+ },
+ ]
+
+ var layout = {
+   polar: {
+     radialaxis: {
+       visible: true,
+       range: donnee.map((d) => d.nombre),
+     }
+   },
+   showlegend: false
+ }
+
+ Plotly.plot("container", data, layout)
+
+   </script>
+ </body>
+ </html>
